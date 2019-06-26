a243b777d6d723e10b1711013b3c177fe2600c59
Cedar <cedarpokemon@gmail.com>
2019-06-26 15:39:57 +0800

    test docs

---
+++ docs/build/precache-manifest.7efcf60932a1d80bcd35f7a401f7fd18.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfcedfb8e3642e20621c9b4a9634a5f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1463711659fed4e28d88",
+    "url": "/static/css/3.b8aef65e.chunk.css"
+  },
+  {
+    "revision": "9ca770dee3c7310896c3",
+    "url": "/static/css/4.37f74549.chunk.css"
+  },
+  {
+    "revision": "c2aff0a5622dc56b8762",
+    "url": "/static/css/5.a0507e5e.chunk.css"
+  },
+  {
+    "revision": "e7906c1fee4af8da588d",
+    "url": "/static/css/main.f1c8b03d.chunk.css"
+  },
+  {
+    "revision": "54530b974f653d270ce8",
+    "url": "/static/js/2.57ec96cc.chunk.js"
+  },
+  {
+    "revision": "1463711659fed4e28d88",
+    "url": "/static/js/3.da6bb694.chunk.js"
+  },
+  {
+    "revision": "9ca770dee3c7310896c3",
+    "url": "/static/js/4.802000bf.chunk.js"
+  },
+  {
+    "revision": "c2aff0a5622dc56b8762",
+    "url": "/static/js/5.6e147557.chunk.js"
+  },
+  {
+    "revision": "ab204947da864b3ba474",
+    "url": "/static/js/6.011efdd7.chunk.js"
+  },
+  {
+    "revision": "2c14affc23a3ee6672c9",
+    "url": "/static/js/7.81334261.chunk.js"
+  },
+  {
+    "revision": "e7906c1fee4af8da588d",
+    "url": "/static/js/main.3f3ae559.chunk.js"
+  },
+  {
+    "revision": "6100a477d2889fd04f99",
+    "url": "/static/js/runtime~main.caba89ff.js"
+  },
+  {
+    "revision": "0f2f9d7f113542e08f1710d64a3aa45d",
+    "url": "/static/media/navi-logo.0f2f9d7f.svg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/react-logo.ee7cd8ed.svg"
+  }
+]);
---
+++ docs/build/static/js/7.81334261.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{58:function(n,t,e){"use strict";e.r(t),t.default={title:"Congratulations on Your New Blog!",tags:["react","navi"],spoiler:"Learn how to add new posts and tweak the theme to your liking.",getContent:function(){return e.e(4).then(e.bind(null,53))}}}}]);
+//# sourceMappingURL=7.81334261.chunk.js.map
